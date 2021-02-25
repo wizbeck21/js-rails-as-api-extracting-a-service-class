c4ef888fa31a51e107a48ad2209ac59a746da5b4
Wesley <wes.beck21@outlook.com>
2021-02-25 10:30:57 -0800

    commit tree dd6df8049a785291bd17fdaf03c4ce708790aba0

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,12 @@
 class SightingsController < ApplicationController
+    def index
+        sightings = Sighting.all
+        render json: SightingSerlializer.new(sightings).to_serialized_json
+    end
+    
     def show
         @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+        render json: SightingSerializer.new(sighting).to_serialized_json
     end
+
 end
